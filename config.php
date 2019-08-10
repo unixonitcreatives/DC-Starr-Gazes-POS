3a72e8923b41d350083cf4fe9d0b1066a28a7885
vincecachi <vincentcachila@gmail.com>
2019-08-10 14:40:30 +0800

    asd

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', '37.59.55.185');//37.59.55.185
-define('DB_USERNAME', 'xAcmiyzWOF'); //xAcmiyzWOF
-define('DB_PASSWORD', 'cQZ2tArDGK'); //cQZ2tArDGK
-define('DB_NAME', 'xAcmiyzWOF');
+define('DB_SERVER', 'localhost');//37.59.55.185
+define('DB_USERNAME', 'root'); //xAcmiyzWOF
+define('DB_PASSWORD', 'root'); //cQZ2tArDGK
+define('DB_NAME', 'dcstarr');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die("Something Went Wrong" . mysqli_connect_error());
--- login.php
+++ login.php
@@ -5,88 +5,83 @@ session_start();
 // Include config file
 require_once "config.php";
 
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: index.php");
-    exit;
-}
-
-
 // Define variables and initialize with empty values
 $username = $password = "";
-$username_err = $password_err = "";
+$alertError = $alertMessage = $username_err = $password_err = $hashed_password = "";
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
+  $username = test_input($_POST['username']);
+  $password = test_input($_POST['password']);
 
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
+  // Validate username and password
+  if(empty($username)){
+      $username_err = "Please enter username.";
+  }
+  if(empty($password)){
+      $password_err = "Please enter password.";
+  }
 
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+  //Query
+  $query="SELECT * FROM users WHERE username='$username' AND password='$password'";
+  $result = mysqli_query($link, $query) or die(mysqli_error($link));
 
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
+  if($result){
 
-            // Set parameters
-            $param_username = $username;
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
+  $rows = mysqli_fetch_array($result);
 
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
 
-                            // Password is correct, so start a new session
-                            session_start();
 
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
+  //Direct pages with different user levels
+  if ($rows['usertype'] == "Administrator") {
 
-                            //Redirect user to
-                            header("location: index.php");
+    session_start();
+    // Store data in session variables
+    $_SESSION["loggedin"] = true;
+    $_SESSION["username"] = $username;
+    $_SESSION["usertype"] = "Administrator";
+    header('location: index.php');
+    exit;
+  }
+  else
+  if ($rows['usertype'] == "Manager") {
+    session_start();
+    // Store data in session variables
+    $_SESSION["loggedin"] = true;
+    $_SESSION["username"] = $username;
+    $_SESSION["usertype"] = "Manager";
+    header('location: index.php');
+    exit;
 
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = "The password you entered was incorrect.";
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
+  }
+  else
+  if ($rows['usertype'] == 'Accounting') {
+    $_SESSION["loggedin"] = true;
+    $_SESSION["username"] = $username;
+    $_SESSION["usertype"] = "Accounting";
+    header('location: index.php');
 
-        // Close statement
-        mysqli_stmt_close($stmt);
-    }
+  }
+  else
+  {
+    // Display an error message
+    $alertError = "No account found with that username or password.";
+  }
 
-    // Close connection
-    mysqli_close($link);
+  // Close connection
+  mysqli_close($link);
+}
+
+
+
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
 ?>
 
@@ -144,7 +139,7 @@ body {
   <!-- /.login-logo -->
   <div class="login-box-body">
 
-    <img class="img-responsive pad" src="dist/img/logo-01.png">
+    <img class="img-responsive pad" src="dist/logo-01.png">
     <p class="login-box-msg">Sign in your credentials</p>
 
 
