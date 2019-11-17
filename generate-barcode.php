7e8e2087474ce98b667be110e94b11d227fb00e9
vincecachi <vincentcachila@gmail.com>
2019-11-17 19:49:51 +0800

    asd

--- generate-barcode.php
+++ generate-barcode.php
@@ -3,9 +3,7 @@
  $text=$_GET['id']; 
 
  //echo "<img alt='testing' src='barcode.php?codetype=Codabar&size=30&text=".$text."&print=true'/>";
-  echo "<img alt='testing' src='barcode.php?size=40&text=".$text."'/>";
-
-
+  echo "<img alt='testing' src='barcode.php?size=40&text=".$text."&print=true'/>";
 
 ?>
 
