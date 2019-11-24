c4b715090031ad6391b432fca91b41eca7c30b6e
Joff <joffpascual@gmail.com>
2019-11-25 07:27:05 +0800

    dasda
    sdsadasd

--- warehouse-add.php
+++ warehouse-add.php
@@ -1,13 +1,13 @@
 <!-- ======================= SESSION =================== -->
 <?php include('template/session.php'); ?>
 <!-- ======================= USER AUTHENTICATION  =================== -->
-<?php 
+<?php
   $Admin_auth = 1;
   $Manager_auth = 0;
   $Cashier_auth = 0;
  include('template/user_auth.php');
 ?>
-<!-- =======================   =================== -->
+
 <?php
 // Define variables and initialize with empty values
 $warehouse_name=$address=$alertMessage="";
@@ -20,7 +20,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     //Assigning posted values to variables.
     $warehouse_name = test_input($_POST['warehouse']);
     $address = test_input($_POST['address']);
-
 
 
 
@@ -61,15 +60,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
                                     $custID = str_pad($newID, 4, '0', STR_PAD_LEFT); //Prepare custom ID with Paddings
                                     $custnewID = $IDtype.$custID; //Prepare custom ID
 
-                                    $query = "INSERT INTO warehouse (custID, warehouse_name, address) 
+                                    $query = "INSERT INTO warehouse (custID, warehouse_name, address)
                                                    VALUES ('$custnewID', '$warehouse_name', '$address')"; //Prepare insert query
 
                                     $result = mysqli_query($link, $query) or die(mysqli_error($link)); //Execute  insert query
-                                    
-                                    
+
+
                                     if($result){
-                                    echo "<script>Notify('new user added succesfully','Success');</script>";
-                                    echo "<script>console.log('new user added');</script>";
+                                    //echo "<script>Notify('new user added succesfully','Success');</script>";
+                                    //echo "<script>console.log('new user added');</script>";
+                                    header("Location: warehouse-manage.php?alert=addsuccess");
                                     }else{
                                       //If execution failed
 
@@ -129,7 +129,7 @@ function test_input($data) {
   <!-- ======================== MAIN CONTENT ======================= -->
     <!-- Main content -->
     <section class="content">
-          
+
           <div class="col-md-6">
           <!-- general form elements -->
           <div class="box box-default">
@@ -150,7 +150,7 @@ function test_input($data) {
                 <div class="form-group">
                   <label>Address</label> <code class="text-orange">Max. 100 characters</code>
                   <textarea class="form-control" rows="3" maxlength="100" id="" oninput="upperCase(this)" placeholder="Enter Address" name="address"></textarea>
-            
+
                 </div>
 
               <!-- /.box-body -->
@@ -177,93 +177,6 @@ function test_input($data) {
 
 <!-- =========================== JAVASCRIPT ========================= -->
       <?php include('template/js.php'); ?>
-
-
-<!-- =========================== PAGE SCRIPT ======================== -->
-
-<!-- Alert animation -->
-<script type="text/javascript">
-$(document).ready(function () {
-
-  window.setTimeout(function() {
-    $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
-      $(this).remove();
-    });
-  }, 1000);
-
-});
-</script>
-
-<script>
-  $(function () {
-    //Initialize Select2 Elements
-    $('.select2').select2()
-
-    //Datemask dd/mm/yyyy
-    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
-    //Datemask2 mm/dd/yyyy
-    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
-    //Money Euro
-    $('[data-mask]').inputmask()
-
-    //Date range picker
-    $('#reservation').daterangepicker()
-    //Date range picker with time picker
-    $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A' })
-    //Date range as a button
-    $('#daterange-btn').daterangepicker(
-      {
-        ranges   : {
-          'Today'       : [moment(), moment()],
-          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        },
-        startDate: moment().subtract(29, 'days'),
-        endDate  : moment()
-      },
-      function (start, end) {
-        $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-      }
-    )
-
-    //Date picker
-    $('#datepicker').datepicker({
-      autoclose: true
-    })
-
-    //iCheck for checkbox and radio inputs
-    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-      checkboxClass: 'icheckbox_minimal-blue',
-      radioClass   : 'iradio_minimal-blue'
-    })
-    //Red color scheme for iCheck
-    $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
-      checkboxClass: 'icheckbox_minimal-red',
-      radioClass   : 'iradio_minimal-red'
-    })
-    //Flat red color scheme for iCheck
-    $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
-      checkboxClass: 'icheckbox_flat-green',
-      radioClass   : 'iradio_flat-green'
-    })
-
-    //Colorpicker
-    $('.my-colorpicker1').colorpicker()
-    //color picker with addon
-    $('.my-colorpicker2').colorpicker()
-
-    //Timepicker
-    $('.timepicker').timepicker({
-      showInputs: false
-    })
-  })
-
-
-</script>
-
 
 
 </body>
