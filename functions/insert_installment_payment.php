b9d2c0cfd07525359e48b6a1762bcdd817851cfc
vincecachi <vincentcachila@gmail.com>
2019-12-16 13:43:55 +0800

    Update insert_installment_payment.php

--- functions/insert_installment_payment.php
+++ functions/insert_installment_payment.php
@@ -30,9 +30,9 @@ if(isset($_POST['paymentBtn'])){
     $y = date('y');
     $d = date('d');
 
-    $qry = mysqli_query($link,"SELECT MAX(id) FROM `installment_history`"); // Get the latest ID
+    $qry = mysqli_query($link,"SELECT MAX(insID) FROM `installment_history`"); // Get the latest ID
     $resulta = mysqli_fetch_array($qry);
-    $newID = $resulta['MAX(id)'] + 1; //Get the latest ID then Add 1
+    $newID = $resulta['MAX(insID)'] + 1; //Get the latest ID then Add 1
     $custID = str_pad($newID, 6, '0', STR_PAD_LEFT); //Prepare custom ID with Paddings
     $custnewID = $IDtype.$m.$d.$y.$custID; //Prepare custom ID
 
